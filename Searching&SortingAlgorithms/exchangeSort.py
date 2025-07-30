27ab60f2103936ce5288129933a54ebd69107571
Prathamesh Karande <148135010+KarandePrathamesh@users.noreply.github.com>
2025-07-30 18:13:43 +0000

    learned exchange sort

---
+++ Searching&SortingAlgorithms/exchangeSort.py
@@ -0,0 +1,11 @@
+def exchangeSort(arr):
+    size = len(arr)-1
+    for i in range(size):
+        for j in range(i+1, size):
+            if(arr[i]>arr[j]):                   # if > sorts ascending , or < sorts descending order
+                arr[i], arr[j] = arr[j], arr[i]
+
+arr = [34,2,5,67,78,89,34,1]
+exchangeSort(arr)
+for i in range(len(arr)-1):
+    print(arr[i],end=' ')
